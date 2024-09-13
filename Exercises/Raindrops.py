b1d1a23f1b6e1879d248eb1833f35e357919c578
Angelito <62396472+Angelous-gif@users.noreply.github.com>
2024-09-13 18:08:51 +0000

    Update and rename archivo.py to Raindrops.py

---
+++ Exercises/Raindrops.py
@@ -0,0 +1,21 @@
+''' Introduction
+Raindrops is a slightly more complex version of the FizzBuzz challenge, a classic interview question.
+Instructions
+Your task is to convert a number into its corresponding raindrop sounds.
+If a given number:
+    is divisible by 3, add "Pling" to the result.
+    is divisible by 5, add "Plang" to the result.
+    is divisible by 7, add "Plong" to the result.
+    is not divisible by 3, 5, or 7, the result should be the number as a string
+Examples
+
+    28 is divisible by 7, but not 3 or 5, so the result would be "Plong".
+    30 is divisible by 3 and 5, but not 7, so the result would be "PlingPlang".
+    34 is not divisible by 3, 5, or 7, so the result would be "34".
+'''
+def convert(number):
+    #Si se puede dividir por 3, agregar Pling al resultado
+    #Si % 5 = 0, agregar Plang
+    # Si La division por 7 da 0 de residuo, agregar Plong al resultado
+    # Si no es divisible por 3, 5 o 7. el resultado deberia ser el nombre como String
+    pass
